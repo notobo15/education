a560aac087db411fe4920838cc7195268a5ab3ca
notobo15 <binh15122003@gmail.com>
2022-08-22 22:03:40 +0700

    First commit

---
+++ main.js
@@ -0,0 +1,53 @@
+
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle
+        ('window-scroll', window.scrollY > 50)
+})
+
+
+
+//show/hide faq anwser
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+
+        //change icon
+        const icon = faq.querySelector('.faq__icon i');
+        if (icon.className === 'uil uil-plus') {
+            icon.className = 'uil uil-minus';
+            faq.style.background = 'var(--color-bg2)';
+        }
+
+        else {
+            icon.className = 'uil uil-plus';
+            faq.style.background = 'var(--color-primary)'
+
+        }
+
+    });
+})
+
+
+//    show/hidde nav menu on <= tablet
+
+const menu = document.querySelector('.nav__menu');
+const menuBtn = document.querySelector('#open-menu-btn');
+const closeBtn = document.querySelector('#close-menu-btn');
+
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = 'flex';
+    closeBtn.style.display = 'inline-block';
+    menuBtn.style.display = 'none';
+});
+
+closeBtn.addEventListener('click', () => {
+    menu.style.display = 'none';
+    closeBtn.style.display = 'none';
+    menuBtn.style.display = 'inline-block';
+});
